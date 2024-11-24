99aca2db296ffaa1fb57ff83c5a28715e98ea92a
Albert Sun <albertsun0@gmail.com>
2024-11-24 12:54:58 -0800

    Init

---
+++ meshgen.gd
@@ -0,0 +1,54 @@
+@tool
+extends MeshInstance3D
+
+
+@export var update = false
+var noise = FastNoiseLite.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	gen_mesh() # Replace with function body.
+
+func get_noise(x:float, y:float):
+	return noise.get_noise_2d(x * 20,y * 20) * 2
+	
+func gen_mesh():
+	var amesh = ArrayMesh.new()
+	var vertices = PackedVector3Array([])
+	var indices = PackedInt32Array([])
+	var N = 4
+	
+	for i in range(N):
+		for j in range(N):
+			vertices.append(Vector3(i,get_noise(i,j),j))
+			if i > 0 and j > 0:
+				var current = N * i + j
+				indices.append_array([
+					current - 1 - N, current, current - N 
+					])
+				indices.append_array([
+					current - 1 - N, current - 1, current
+					])
+	var array = []
+	array.resize(Mesh.ARRAY_MAX)
+	array[Mesh.ARRAY_VERTEX] = vertices
+	array[Mesh.ARRAY_INDEX] = indices
+	amesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, array)
+	mesh = amesh
+	
+	var surftool = SurfaceTool.new()
+	
+	surftool.begin(Mesh.PRIMITIVE_TRIANGLES)
+	for i in range(vertices.size()):
+		surftool.add_vertex(vertices[i])
+	for i in indices:
+		surftool.add_index(i)
+	surftool.generate_normals()
+	amesh = surftool.commit()
+	mesh = amesh
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if update:
+		gen_mesh()
+		update = false
